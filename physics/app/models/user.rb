918845446fb45d93f8138de78ab236cb9237f737
Liang <zhoul@mit.edu>
2015-01-15 23:49:11 -0500

    setup basic pages

---
+++ physics/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  validates :username, presence: true, length: { maximum: 20 }, uniqueness: {case_sensitive: false}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 40 }, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false }
+
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+end
