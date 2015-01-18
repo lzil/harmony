0a4903c467bf9515eb4edb0012c2b240662e0bc7
Liang <zhoul@mit.edu>
2015-01-18 03:17:23 -0500

    share

--- physics/app/controllers/users_controller.rb
+++ physics/app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Congratulations! You have successfully signed up <3"
+      redirect_to @user
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  def edit
+    @article = Article.find(user_params)
+  end
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end 
 end
+
--- physics/config/routes.rb
+++ physics/config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+  get 'signup' => 'static_pages#home'
 
   root 'static_pages#home'
 
   get 'about' => 'static_pages#about'
 
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
