bfeaaa3120d0505c544e2875c5fe254e9f8f6b3a
Liang <zhoul@mit.edu>
2015-01-19 02:12:21 -0500

    stylized login/signup

--- physics/app/controllers/sessions_controller.rb
+++ physics/app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
   def new
-
+    render 'static_pages/editor'
   end
 
   def create
     user = User.find_by(username: params[:session][:username].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      redirect_to dashboard_path
     else
       flash.now[:danger] = "Invalid username/password combo. Try again!!!!"
       render 'static_pages/home'
