a01e079b22078300ceca26edd58d933502418f2c
Liang <zhoul@mit.edu>
2015-01-18 22:49:11 -0500

    hi monde

--- physics/config/routes.rb
+++ physics/config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'login' => 'sessions#new'
-  post 'login' => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
-
-  get 'signup' => 'static_pages#home'
-
-  root 'static_pages#home'
-
-  get 'about' => 'static_pages#about'
-
+  root      'static_pages#home'
+  get       'login'      => 'sessions#new'
+  post      'login'      => 'sessions#create'
+  delete    'logout'     => 'sessions#destroy'
+  get       'signup'     => 'static_pages#home'
+  get       'about'      => 'static_pages#about'
+  get       'editor'     => 'static_pages#editor'
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
