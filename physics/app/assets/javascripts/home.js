fb1f6579761e6ce990fa1c4e96ae0d2f6d41272c
Liang <zhoul@mit.edu>
2015-01-24 22:53:19 -0500

    more stuff

---
+++ physics/app/assets/javascripts/home.js
@@ -0,0 +1,54 @@
+var home = function() {
+	$('.login-box').hide();
+	$('.signup-box').hide();
+	if($('.login-fail').text() === '') {
+		$('.login-fail').hide();
+	};
+	if($('.signup-fail').text() === '') {
+		$('.signup-fail').hide();
+	};
+	$('.login-tab').click(function() {
+		if(!$('.login-box').is(':visible')) {
+			$('.login-tab').animate({opacity:0.25}, 300)
+			if($('.signup-box').is(':visible')) {
+				$('.signup-box').fadeOut(150);
+				$('.signup-tab').animate({opacity:1}, 300)
+				$('.login-tab').animate({opacity:0.25}, 300)
+				setTimeout(function() {
+					$('.signup-box').hide();
+					$('.login-box').fadeIn(150);
+				}, 150);
+			}
+			else {
+				$('.login-box').fadeIn(300);
+			}
+		}
+		else {
+			$('.login-tab').animate({opacity:1}, 300)
+			$('.login-box').fadeOut(300);
+		}
+	});
+	$('.signup-tab').click(function() {
+		if(!$('.signup-box').is(':visible')) {
+			if($('.login-box').is(':visible')) {
+				$('.login-box').fadeOut(150);
+				$('.login-tab').animate({opacity:1}, 300)
+				$('.signup-tab').animate({opacity:0.25}, 300)
+				setTimeout(function() {
+					$('.login-box').hide();
+					$('.signup-box').fadeIn(150);
+				}, 150);
+			}
+			else {
+				$('.signup-box').fadeIn(300);
+				$('.signup-tab').animate({opacity:0.25}, 300)
+			}
+		}
+		else {
+			$('#signup-box').animate({opacity:1}, 300)
+			$('.signup-box').fadeOut(300);
+		}
+	});
+};
+
+$(document).ready(home);
